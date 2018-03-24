6e5ca4b1fca48ee3406a0557ac3c002c58a6815f
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2018-03-24 17:11:12 +0100

    Initial Commit

---
+++ BeagleIM/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BeagleIM
+//
+//  Created by Andrzej Wójcik on 24.03.2018.
+//  Copyright © 2018 HI-LOW. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
