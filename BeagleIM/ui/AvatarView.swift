8888f19b5a453f24af5426dc55449362ce3075f0
Andrzej Wójcik <andrzej@hi-low.eu>
2018-10-04 21:01:54 +0200

    Fixed issue with avatar aspect ratio

--- BeagleIM/ui/AvatarView.swift
+++ BeagleIM/ui/AvatarView.swift
@@ -9,14 +9,21 @@
 import AppKit
 
 class AvatarView: NSImageView {
-        
+
     override func draw(_ dirtyRect: NSRect) {
         NSGraphicsContext.saveGraphicsState();
         
         let path = NSBezierPath(roundedRect: dirtyRect, xRadius: frame.width/2, yRadius: frame.width/2);
         path.addClip();
         
-        image?.draw(in: dirtyRect, from: .zero, operation: .sourceOver, fraction: 1.0);
+        if let image = self.image {
+            let size = image.size;
+            let widthDiff = max(size.width - size.height, 0) / 2;
+            let heightDiff = max(size.height - size.width, 0) / 2;
+            let width = size.width - (2 * widthDiff);
+            let height = size.height - (2 * heightDiff);
+            image.draw(in: dirtyRect, from: NSRect(x: widthDiff, y: heightDiff, width: width, height: height), operation: .sourceOver, fraction: 1.0);
+        }
         
         NSGraphicsContext.restoreGraphicsState();
     }
