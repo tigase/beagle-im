150a98b685bf6b68594ccecfcf12f1e20820f5d8
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2020-03-28 13:04:46 +0100

    Improve checks for links before showing link previews #beagleim-225

--- BeagleIM/database/DBChatHistoryStore.swift
+++ BeagleIM/database/DBChatHistoryStore.swift
@@ -324,12 +324,15 @@ class DBChatHistoryStore {
             }
         }
         if linkPreviewAction != .none && type == .message, #available(macOS 10.15, *) {
+            DispatchQueue.global(qos: .background).async {
             // if we may have previews, we should add them here..
             if let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue | NSTextCheckingResult.CheckingType.address.rawValue) {
                 let matches = detector.matches(in: data, range: NSMakeRange(0, data.utf16.count));
                 matches.forEach { match in
                     if let url = match.url, let scheme = url.scheme, ["https", "http"].contains(scheme) {
-                        DBChatHistoryStore.instance.appendItem(for: account, with: jid, state: state, authorNickname: authorNickname, authorJid: authorJid, recipientNickname: recipientNickname, participantId: participantId, type: .linkPreview, timestamp: timestamp, stanzaId: nil, serverMsgId: nil, remoteMsgId: nil, data: url.absoluteString, encryption: .none, encryptionFingerprint: nil, linkPreviewAction: .none, completionHandler: nil);
+                        if (data as NSString).range(of: "http", options: .caseInsensitive, range: match.range).location == match.range.location {
+                            DBChatHistoryStore.instance.appendItem(for: account, with: jid, state: state, authorNickname: authorNickname, authorJid: authorJid, recipientNickname: recipientNickname, participantId: participantId, type: .linkPreview, timestamp: timestamp, stanzaId: nil, serverMsgId: nil, remoteMsgId: nil, data: url.absoluteString, encryption: .none, encryptionFingerprint: nil, linkPreviewAction: .none, completionHandler: nil);
+                        }
                     }
                     if let address = match.components {
                         let query = address.values.joined(separator: ",").addingPercentEncoding(withAllowedCharacters: .urlHostAllowed);
@@ -337,6 +340,7 @@ class DBChatHistoryStore {
                         DBChatHistoryStore.instance.appendItem(for: account, with: jid, state: state, authorNickname: authorNickname, authorJid: authorJid, recipientNickname: recipientNickname, participantId: participantId, type: .linkPreview, timestamp: timestamp, stanzaId: nil, serverMsgId: nil, remoteMsgId: nil, data: mapUrl.absoluteString, encryption: .none, encryptionFingerprint: nil, linkPreviewAction: .none, completionHandler: nil);
                     }
                 }
+            }
             }
         }
     }
